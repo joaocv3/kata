6570bf5d3e486aaaf7f8f00d43272bb225a385d8
Joao C Vieira <joaocv3@gmail.com>
2017-07-24 21:20:26 -0300

    Fifth case: returns sum result for numbers also separated by new lines

--- 1/lib/string_calculator.rb
+++ 1/lib/string_calculator.rb
@@ -7,10 +7,18 @@ class StringCalculator
     return 0 if @numbers == ""
     return @numbers.to_i unless @numbers.include? ","
 
+    numbers = to_array(@numbers)
+
     total = 0
-    @numbers.split(",").each do |number|
+    numbers.each do |number|
       total += number.to_i
     end
     total
   end
+
+  def to_array(numbers)
+    numbers.split(/[^0-9]/)
+  end
 end
+
+#StringCalculator.new("32,23,123,132\n312").calc
--- 1/spec/string_calculator_spec.rb
+++ 1/spec/string_calculator_spec.rb
@@ -32,4 +32,22 @@ RSpec.describe StringCalculator do
       expect(result).to eq  34
     end
   end
+
+  context "with more than 2 numbers" do
+    context "with only commas to separate numbers" do
+      result = StringCalculator.new("3, 4, 23, 4").calc
+
+      it "returns the sum result" do
+        expect(result).to eq  34
+      end
+    end
+
+    context "with commas and/or new lines to separate numbers" do
+      result = StringCalculator.new("7, 4\n 23\n 1").calc
+
+      it "returns the sum result" do
+        expect(result).to eq  35
+      end
+    end
+  end
 end
